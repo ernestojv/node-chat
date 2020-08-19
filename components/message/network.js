282addcce747052c2659c7bd40c5acee91bc1066
Ernesto Vanegas <ernestovanegas9@gmail.com>
2020-08-19 01:53:14 -0600

    Carga de archivos

--- components/message/network.js
+++ components/message/network.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const response = require('../../network/response');
 const controller = require('./controller');
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now() +
+            path.extname(file.originalname))
+    }
+});
+const upload = multer({ storage: storage });
 
 router.get('/', async (req, res) => {
     try {
@@ -15,9 +26,9 @@ router.get('/', async (req, res) => {
 
 });
 
-router.post('/', async (req, res) => {
+router.post('/', upload.single('file'), async (req, res) => {
     try {
-        const result = await controller.addMessage(req.body.chat ,req.body.user, req.body.message);
+        const result = await controller.addMessage(req.body.chat, req.body.user, req.body.message);
         response.succes(req, res, result, 201);
     } catch (e) {
         response.error(req, res, 'Invalid data', 400, e);
@@ -26,20 +37,20 @@ router.post('/', async (req, res) => {
 
 });
 
-router.patch('/:id', async (req, res) =>{
-    try{
+router.patch('/:id', async (req, res) => {
+    try {
         const result = await controller.updateMessage(req.params.id, req.body.message);
         response.succes(req, res, result, 201);
-    }catch(e){
+    } catch (e) {
         response.error(req, res, 'Internal error', 500, e);
     }
 });
 
-router.delete('/:id', async(req, res)=>{
-    try{
+router.delete('/:id', async (req, res) => {
+    try {
         const result = await controller.deleteMessage(req.params.id);
         response.succes(req, res, `Mensagge: ${req.params.id} deleted`);
-    }catch(e){
+    } catch (e) {
         response.error(req, res, 'Internal server error', 500, e);
     }
 })
