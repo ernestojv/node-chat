4c1e3bf75e9b43711643980ac9935e96f8c5b737
Ernesto Vanegas <ernestovanegas9@gmail.com>
2020-08-23 15:48:00 -0600

    Vercel

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 const server = require('http').Server(app);
 const db = require('./db');
 const config = require('./config');
+const swaggerDoc = require('./swagger.json');
 db.connect();
+
 app.use('/app', express.static('public'));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 const socket = require('./socket');
 const router = require('./network/routes');
 
