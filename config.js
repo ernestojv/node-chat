f333d8128b53ea7776518cb6e753b722b9c1012b
Ernesto Juárez <59668691+ernestojv@users.noreply.github.com>
2020-08-24 02:30:31 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://db_nodechat:Ernesto07@cluster0-yuddw.mongodb.net/node-chat?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://<db_name>:<password>@<cluster>.mongodb.net/<db_name>?retryWrites=true&w=majority',
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'http://localhost',
     fileUrl: process.env.FILE_URL || 'http://localhost:3000/app/files/',
